aaf4d16c868580ba49acee6b516799cac44bb74f
Tejas R <git config --global user.name tejasrt45@gmail.com>
2025-04-05 20:32:25 +0530

    first commit

---
+++ fullstacktradingApp/src/main/java/com/chaos/fullstacktradingApp/FullstacktradingAppApplication.java
@@ -0,0 +1,13 @@
+package com.chaos.fullstacktradingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstacktradingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstacktradingAppApplication.class, args);
+	}
+
+}
